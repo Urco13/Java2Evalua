ae97af9b704f3a6f1af890039975ac363815fbd8
Carlos <urco13@gmail.com>
2017-01-19 10:57:58 +0100

    Add files via upload

---
+++ EvaPrueba1String/src/evaprueba1string/EvaPrueba1String.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evaprueba1string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author dam115
+ */
+public class EvaPrueba1String {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+        String cad =new String("Cadena fija");
+        System.out.println("Ejemplo de String '"+cad+"'");
+        System.out.println("Su longitud es "+cad.length());
+        System.out.println("Su tercer caracter es "+cad.charAt(2));
+    }//fin main
+    
+   
+}//fin class
