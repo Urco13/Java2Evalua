955a6870abd9c76e71b3202ffd0cbae9786e1795
Carlos <urco13@gmail.com>
2017-01-26 10:55:47 +0100

    Add files via upload

---
+++ EvaNombreOrdenador/src/evanombreordenador/EvaNombreOrdenador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evanombreordenador;
+
+/**
+ *
+ * @author dam115
+ */
+public class EvaNombreOrdenador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+    
+}
---
+++ EvaNombreOrdenador/src/evanombreordenador/NombreOrdenador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evanombreordenador;
+
+/**
+ *
+ * @author dam115
+ */
+public class NombreOrdenador {
+    private String etiqueta;
+
+    public NombreOrdenador(String identificacion) {
+        String formato="PC";
+        String [] vector={"5","16","18","25","20","21"};
+        if(formato==identificacion.substring(0,2));
+        for (int i = 0; i < vector.length; i++) {
+            if (vector[i]==identificacion.substring(i)) {
+                
+            }
+    
+        }
+        
+    }
+    
+}//fin class
