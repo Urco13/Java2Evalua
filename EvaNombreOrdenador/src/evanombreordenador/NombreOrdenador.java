af3f14cf0b95542e06c778cba749dc49b1955e29
Carlos <urco13@gmail.com>
2017-01-29 12:10:51 +0100

    Update NombreOrdenador.java

--- EvaNombreOrdenador/src/evanombreordenador/NombreOrdenador.java
+++ EvaNombreOrdenador/src/evanombreordenador/NombreOrdenador.java
@@ -10,19 +10,55 @@ package evanombreordenador;
  * @author dam115
  */
 public class NombreOrdenador {
-    private String etiqueta;
-
-    public NombreOrdenador(String identificacion) {
-        String formato="PC";
-        String [] vector={"5","16","18","25","20","21"};
-        if(formato==identificacion.substring(0,2));
-        for (int i = 0; i < vector.length; i++) {
-            if (vector[i]==identificacion.substring(i)) {
-                
-            }
+    private String codigo;
     
-        }
+    public NombreOrdenador(String codigo){
+        String trozoCadena1="PC";
+        String [] vector={"05","16","18","25","20","21"};
+        String trozoCadena2;
+        String trozoCadena3;
+        boolean resultado=true;
+        int numero;
         
+        if(codigo.length()==6){   
+        
+        if(trozoCadena1.compareTo(codigo.substring(0, 2))==0){  
+        }else{
+            resultado=false;
+        }
+        trozoCadena2=codigo.substring(2, 4);
+         for (int i = 0; i < vector.length; i++) {
+                if(vector[i].compareTo(codigo.substring(2, 4))==0){
+                    break;
+                }else{
+                    resultado=false;
+                }
+          }
+          trozoCadena3=codigo.substring(4, 6);
+          for (int i = 0; i < trozoCadena3.length(); i++) {
+              if(Character.isDigit(trozoCadena3.charAt(i))){
+              }else{
+                  resultado=false;
+              }
+          }
+          numero=Integer.parseInt(trozoCadena3);
+          if(numero>0 && numero<31){
+          }else{
+              resultado=false;
+          }
+          if(resultado){
+              System.out.println("Codigo Correcto");
+              this.codigo=trozoCadena1+vector+trozoCadena3;
+          }else
+              System.out.println("Codigo Erroneo");
+         }else{
+            System.out.println("La longitud del codigo es incorrecta");
+        }
+       
+    }//fin nombreOrdenador
+
+    public String getCodigo() {
+        return codigo;
     }
     
 }//fin class
