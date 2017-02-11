9d433d4d0ae6091a3b47f0757167c00229f8572b
Carlos <urco13@gmail.com>
2017-02-11 23:44:33 +0100

    Add files via upload

--- EvaCadenas3MontarNombres/src/evacadenas3montarnombres/EvaCadenas3MontarNombres.java
+++ EvaCadenas3MontarNombres/src/evacadenas3montarnombres/EvaCadenas3MontarNombres.java
@@ -8,10 +8,9 @@ package evacadenas3montarnombres;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-
 /**
  *
- * @author dam115
+ * @author urco1
  */
 public class EvaCadenas3MontarNombres {
 
@@ -22,20 +21,27 @@ public class EvaCadenas3MontarNombres {
     public static void main(String[] args) {
         String nombre, ape1, ape2;
         String miNombre=new String();//Creamos el objeto mi nombre
+        do{
         nombre=leerTeclado("Dime tu nombre");
-        ape1=leerTeclado("Dime tu primer apellido");
-        ape2=leerTeclado("Dime tu segundo apellido");
+        }while(!comprobacion(nombre));
+        do {
+             ape1=leerTeclado("Dime tu primer apellido");
+        } while (!comprobacion(ape1));
+        do {
+            ape2=leerTeclado("Dime tu segundo apellido");
+        } while (!comprobacion(ape2));
+        
         miNombre=nombre+" "+ape1+" "+ape2;//juntamos las 3 cadenas y la metemos en la cadena miNombre
         System.out.println(miNombre);
-        for (int i = 0; i < miNombre.length(); i++) {//recorremos la cadena
-            if(!Character.isLetter(miNombre.charAt(i))){//buscamos si hay algun caracter que no sea del nombre
-                System.out.println("Esto no es una letra "+miNombre.charAt(i));
-            }
-            
-        }
-        if(miNombre.isEmpty()){//el metodo isEmpty comprueba si hay campos vacios
-            System.out.println("No se permite campos vacios");
-        }
+//        for (int i = 0; i < miNombre.length(); i++) {//recorremos la cadena
+//            if(!Character.isLetter(miNombre.charAt(i))){//buscamos si hay algun caracter que no sea del nombre
+//                System.out.println("Esto no es una letra "+miNombre.charAt(i));
+//            }
+//            
+//        }
+//        if(miNombre.isEmpty()){//el metodo isEmpty comprueba si hay campos vacios
+//            System.out.println("No se permite campos vacios");
+//        }
         
         
         
@@ -52,15 +58,22 @@ public class EvaCadenas3MontarNombres {
             System.out.println("Error");
         }
         return cadena;
-    }
-//    public static void comprobacion(String cadena){
-//        for (int i = 0; i < cadena.length(); i++) {
-//            if((cadena.charAt(i)>='a'&&cadena.charAt(i)<='z')||(cadena.charAt(i)>='A'&&cadena.charAt(i)<='Z')||!cadena.isEmpty()){
-//                
-//            }else{
-//                System.out.println("Error");
-//            }
-//        }
-//        System.out.println("Tu nombre es correcto puedes continuar\n");
-//    }
+    }//fin leerTeclado
+    
+    public static boolean comprobacion(String cadena){
+        boolean correcto = true;
+        for (int i = 0; i < cadena.length(); i++) {
+            if(!((cadena.charAt(i)>='a'&&cadena.charAt(i)<='z')||(cadena.charAt(i)>='A'&&cadena.charAt(i)<='Z')||cadena.charAt(i)==' ')){
+                correcto=false;
+                System.out.println("Este caracter "+cadena.charAt(i)+" no es correcto intentalo otra vez");
+                break;  
+            }
+        }
+            if (cadena.isEmpty()) {
+            System.out.println("Tu nombre no es correcto vuelve a intentarlo");
+            correcto=false;
+        }
+
+        return correcto;
+    }//fin comprobacion
 }//fin class
